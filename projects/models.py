dd10727553eeca6aa571873d992fa45e5e92c9e6
Lenny Makori <makorilenny28@gmail.com>
2020-06-08 14:38:03 +0300

    install pillow and create super user

--- projects/models.py
+++ projects/models.py
@@ -5,3 +5,10 @@ class Profile(models.Model):
     profile_pic = models.ImageField(upload_to='image/', default="image/male.png", blank=True)
     bio = models.TextField(blank=True)
     contact_info = models.CharField(max_length=60)
+
+
+class Project(models.Model):
+    project_image = models.ImageField(upload_to='image/')
+    project_title = models.CharField(max_length=60)
+    project_description = models.TextField()
+    project_link = models.CharField(max_length=150)
