6d034940ee31deedd9c8243129fc7360cb8cc60a
Lenny Makori <makorilenny28@gmail.com>
2020-06-08 19:29:15 +0300

    create update-profile view function

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Project, Profile
+from .forms import ProfileForm
 
 # Create your views here.
 
@@ -11,3 +13,19 @@ def profile(request, user_id):
     user_profile = Profile.get_profile(user_id)
 
     return render(request, 'profile.html', {'user_profile':user_profile})
+
+def update_profile(request, user_id):
+    current_user = request.user
+    current_user_id = current_user.id
+    user_profile = Profile.get_profile(user_id)
+    
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=user_profile)
+        if form.is_valid():
+            form.save()
+
+        return redirect('homepage')
+    else:
+        form = ProfileForm()
+
+    return render(request, 'profile_edit.html', {"form": form})
