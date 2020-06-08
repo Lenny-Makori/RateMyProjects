f07aaccf96c9d077ce831e55f15793931155fa27
Lenny Makori <makorilenny28@gmail.com>
2020-06-08 19:22:07 +0300

    import User model

--- projects/models.py
+++ projects/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
     profile_pic = models.ImageField(upload_to='image/', default="image/male.png", blank=True)
     bio = models.TextField(blank=True)
     contact_info = models.CharField(max_length=60)
@@ -12,3 +14,5 @@ class Project(models.Model):
     project_title = models.CharField(max_length=60)
     project_description = models.TextField()
     project_link = models.CharField(max_length=150)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
--- projects/views.py
+++ projects/views.py
@@ -6,3 +6,8 @@ def index(request):
     test = "Nakucheki nakudigi"
 
     return render(request, 'mainview/mainpage.html', {"image_display":test})
+
+def profile(request, user_id):
+    user_profile = Profile.get_profile(user_id)
+
+    return render(request, 'profile.html', {'user_profile':user_profile})
