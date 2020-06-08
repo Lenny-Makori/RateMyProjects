0e6be93aacccb9976ca3853c3701decfe22aa906
Lenny Makori <makorilenny28@gmail.com>
2020-06-08 13:59:15 +0300

    create profile model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to='image/', default="image/male.png", blank=True)
+    bio = models.TextField(blank=True)
+    contact_info = models.CharField(max_length=60)
