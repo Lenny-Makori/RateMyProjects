120cb165a0e199b69148e7036f4861efb5784d42
Lenny Makori <makorilenny28@gmail.com>
2020-06-09 18:09:18 +0300

    create project api view function

--- projects/views.py
+++ projects/views.py
@@ -87,3 +87,9 @@ class ProfileList(APIView):
         all_merch = Profile.objects.all()
         serializers = ProfileSerializer(all_merch, many=True)
         return Response(serializers.data)
+
+class ProjectList(APIView):
+    def get(self, request, format=None):
+        all_merch = Project.objects.all()
+        serializers = ProjectSerializer(all_merch, many=True)
+        return Response(serializers.data)
